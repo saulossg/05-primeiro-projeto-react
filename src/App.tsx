57db13a837d1e9a9a18fc0031241c05cbc1dc375
Saulo Gomes <saulo.gomes@tbfg.com.br>
2020-06-15 00:26:20 -0300

    Fix eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
-import './App.css';
+import Routes from './routes';
+import { BrowserRouter } from 'react-router-dom';
 
-function App() {
-  return <div>Hello World...</div>
-}
+const App: React.FC = () => (
+  <BrowserRouter>
+    <Routes />
+  </BrowserRouter>
+);
 
 export default App;
